24bdc3c976fcc898ac26673d44219bef72b49982
keithsjohnson <keithsjohnson@yahoo.com>
2015-08-15 17:13:40 +0100

    Add Test and change not to use project settings

---
+++ PostcodeLocationDisplayer/src/test/java/uk/co/keithsjohnson/postcode/displayer/PostcodeLocationDisplayerMainApplicationTest.java
@@ -0,0 +1,15 @@
+package uk.co.keithsjohnson.postcode.displayer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PostcodeLocationDisplayerMainApplication.class)
+public class PostcodeLocationDisplayerMainApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
---
+++ PostcodeLocationRequestReceiver/src/test/java/uk/co/keithsjohnson/postcode/location/request/receiver/PostcodeLocationRequestReceiverApplicationTest.java
@@ -0,0 +1,15 @@
+package uk.co.keithsjohnson.postcode.location.request.receiver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PostcodeLocationRequestReceiverApplication.class)
+public class PostcodeLocationRequestReceiverApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
