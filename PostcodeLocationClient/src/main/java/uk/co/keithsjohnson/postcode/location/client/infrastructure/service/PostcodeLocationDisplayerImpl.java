aaf1d2262abfb3ca9bffcb27ea4656e7daf14776
keithsjohnson <keithsjohnson@yahoo.com>
2015-08-20 21:29:37 +0100

    Tidy map.html

--- PostcodeLocationClient/src/main/java/uk/co/keithsjohnson/postcode/location/client/infrastructure/service/PostcodeLocationDisplayerImpl.java
+++ PostcodeLocationClient/src/main/java/uk/co/keithsjohnson/postcode/location/client/infrastructure/service/PostcodeLocationDisplayerImpl.java
@@ -1,8 +1,5 @@
 package uk.co.keithsjohnson.postcode.location.client.infrastructure.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -18,11 +15,7 @@ public class PostcodeLocationDisplayerImpl implements PostcodeLocationDisplayer 
 
 	@Override
 	public void sendPostcodeLocationForDisplayOnMap(PostcodeLocation postcodeLocation) {
-		final Map<String, String> msg = new HashMap<>();
-		msg.put("postcode", postcodeLocation.getPostcode());
-		msg.put("latitude", postcodeLocation.getLatitude());
-		msg.put("longitude", postcodeLocation.getLongitude());
-		this.messaging.convertAndSend("/topic/postcodemap", msg);
+		this.messaging.convertAndSend("/topic/postcodemap", postcodeLocation);
 	}
 
 }
